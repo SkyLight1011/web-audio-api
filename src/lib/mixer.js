af6e2a85261f0fe1bbf365467bf78020286017a0
Konstantin Volkov <volko@ciklum.com>
2017-11-23 12:28:15 +0200

    Mixer tracks refactoring

--- src/lib/mixer.js
+++ src/lib/mixer.js
@@ -7,9 +7,13 @@ export class MixerNode extends GainNode {
     super(ctx);
 
     this._tracks = [];
+  }
 
-    for (let i = 0; i++ < 24;) {
-      let track = {
+  getTrack(trackNo = 1, create) {
+    let track = this._tracks[trackNo - 1];
+
+    if (!track && create) {
+      track = {
         instrument: null,
         fx: [this.context.createGain()],
         master: this.context.createGain()
@@ -21,10 +25,12 @@ export class MixerNode extends GainNode {
 
       this._tracks.push(track);
     }
+
+    return track;
   }
 
   assignInstrument(instrument, trackNo = 1) {
-    let track = this._tracks[trackNo - 1];
+    let track = this.getTrack(trackNo, true);
 
     if (track.instrument) {
       track.instrument.cut();
@@ -35,7 +41,7 @@ export class MixerNode extends GainNode {
   }
 
   addFx(fxNode, trackNo = 1) {
-    let track = this._tracks[trackNo - 1];
+    let track = this.getTrack(trackNo, true);
     let lastFx = track.fx[track.fx.length - 1];
 
     lastFx.cut();
@@ -45,6 +51,6 @@ export class MixerNode extends GainNode {
   }
 
   setVolume(volume, trackNo = 1) {
-    this._tracks[trackNo - 1].master.gain.value = volume;
+    this.getTrack(trackNo, true).master.gain.value = volume;
   }
 }
