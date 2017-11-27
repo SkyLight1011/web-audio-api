060b22c78d1bde5f3672cddad2d43012cd64b7a9
Konstantin Volkov <volko@ciklum.com>
2017-11-27 13:06:16 +0200

    Added stereo effect to Delay

--- src/lib/feedback-delay.js
+++ src/lib/feedback-delay.js
@@ -8,28 +8,64 @@ export class FeedbackDelayNode extends GainNode {
 
     let delay = this.context.createDelay();
     let feedback = this.context.createGain();
+    let cutoff;
 
     this._preset = Object.assign({
       delay: 0.5,
       feedback: 0.5,
-      cutoff: 2000
+      cutoff: 2000,
+      stereo: 0
     }, preset);
     this._output = this.context.createGain();
 
     feedback.gain.value = this._preset.feedback;
     delay.delayTime.value = this._preset.delay;
 
-    this.connect(this._output);
-    this.to(delay).to(feedback);
-
     if (this._preset.cutoff) {
-      let filter = this.context.createBiquadFilter();
+      cutoff = this.context.createBiquadFilter();
 
-      filter.frequency.value = this._preset.cutoff;
+      cutoff.frequency.value = this._preset.cutoff;
+    }
 
-      feedback.to(filter).to(this._output);
+    this.connect(this._output);
+
+    if (this._preset.stereo) {
+      let stereo = Math.abs(this._preset.stereo);
+      let delayR = this.context.createDelay();
+      let feedbackR = this.context.createGain();
+      let splitter = this.context.createChannelSplitter(2);
+      let merger = this.context.createChannelMerger(2);
+      let isTrueStereo = false;
+
+      delayR.delayTime.value = this._preset.delay;
+
+      splitter.connect(delay, 0);
+
+      if (isTrueStereo) { // For future inprovements
+        splitter.connect(delayR, 1);
+      }
+
+      feedbackR.gain.value = this._preset.feedback;
+
+      if (cutoff) {
+        delay.to(feedback).to(cutoff).to(delayR).to(feedbackR).to(delay);
+      } else {
+        delay.to(feedback).to(delayR).to(feedbackR).to(delay);
+      }
+
+      feedback.connect(merger, 0, 0);
+      feedbackR.connect(merger, 0, 1);
+
+      this.connect(splitter);
+      merger.to(this._output);
     } else {
-      feedback.to(this._output);
+      this.to(delay).to(feedback);
+
+      if (cutoff) {
+        feedback.to(filter).to(this._output);
+      } else {
+        feedback.to(this._output);
+      }
     }
   }
 }
