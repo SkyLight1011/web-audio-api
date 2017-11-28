96a1e41ae01077cc35329f97134562bc59509ba8
Konstantin Volkov <volko@ciklum.com>
2017-11-28 18:02:39 +0200

    Save node connection targets internally

--- src/lib/overrides.js
+++ src/lib/overrides.js
@@ -9,3 +9,27 @@ AudioNode.prototype.to = function (...targets) {
 AudioNode.prototype.cut = function (target) {
   (this._output || this).disconnect(target && target._input || target);
 };
+
+let connect = AudioNode.prototype.connect;
+let disconnect = AudioNode.prototype.disconnect;
+
+AudioNode.prototype.connect = function (target) {
+  this._targets || (this._targets = []);
+  this._targets.push(target);
+
+  connect.call(this, ...arguments);
+};
+
+AudioNode.prototype.disconnect = function (target) {
+  if (!target) {
+    this._targets = [];
+  } else {
+    let index = (this._targets || []).indexOf(target);
+
+    if (index > -1) {
+      this._targets.splice(index, 1);
+    }
+  }
+
+  disconnect.call(this, ...arguments);
+}
