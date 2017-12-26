4944500b6d22cad4fec52cce541d66bcfb4545ae
Konstantin Volkov <volko@ciklum.com>
2017-12-26 17:21:14 +0200

    Revert plugin system for now

--- src/lib/core/plugin/index.js
+++
@@ -1,6 +0,0 @@
-import {CommonPlugin} from './common-plugin.js';
-import {SourcePlugin} from './source-plugin.js';
-import {FxPlugin} from './fx-plugin.js';
-import {InstrumentPlugin} from './instrument-plugin.js';
-
-export {CommonPlugin, SourcePlugin, FxPlugin, InstrumentPlugin};
--- src/lib/core/plugin/params/index.js
+++
@@ -1,5 +0,0 @@
-import {CommonParam} from './common-param.js';
-import {OptionsParam} from './options-param.js';
-import {BooleanParam} from './boolean-param.js';
-
-export {CommonParam, OptionsParam, BooleanParam};
