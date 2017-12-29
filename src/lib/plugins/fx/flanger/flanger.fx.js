120f27131e19194be8e7c8fa01a32dbedd854c06
Konstantin Volkov <volko@ciklum.com>
2017-12-29 16:40:48 +0200

    Added Flanger fx

---
+++ src/lib/plugins/fx/flanger/flanger.fx.js
@@ -0,0 +1,65 @@
+import {FxPlugin} from '../../../fx-plugin.js';
+
+export class FlangerFX extends FxPlugin {
+  constructor(...args) {
+    super(...args);
+  }
+
+  static get id() {
+    return 'flanger';
+  }
+
+  get defaults() {
+    return Object.assign({}, super.defaults, {
+      delay: {
+        name: 'Delay',
+        unit: 's',
+        min: 0,
+        max: 5,
+        default: 0.005
+      },
+      depth: {
+        name: 'Depth',
+        min: 0,
+        max: 1,
+        default: 0.002
+      },
+      feedback: {
+        name: 'Feedback',
+        min: 0,
+        max: 1,
+        default: 0.5
+      },
+      speed: {
+        name: 'Speed',
+        min: 0,
+        max: 5,
+        default: 0.25
+      }
+    });
+  }
+
+  setup() {
+    super.setup();
+
+    this._osc = this.context.createGenerator({
+      type: 'sine',
+      frequency: this.get('speed'),
+      gain: this.get('depth')
+    });
+    this._delay = this.context.createDelay();
+    this._wet = this.context.createGain();
+    this._feedback = this.context.createGain();
+
+    this._delay.delayTime.set(this.get('delay'));
+    this._feedback.gain.set(this.get('feedback'));
+
+    this._osc.to(this._delay.delayTime);
+    this._fx
+      .to(this._wet, this._delay)
+      .to(this._output);
+    this._delay
+      .to(this._feedback, this._wet)
+      .to(this._fx);
+  }
+}
--- src/lib/plugins/fx/index.js
+++ src/lib/plugins/fx/index.js
@@ -1,9 +1,11 @@
 import {DelayFX} from './delay/delay.fx.js';
 import {ReverbFX} from './reverb/reverb.fx.js';
 import {ParametreqFX} from './parametreq/parametreq.fx.js';
+import {FlangerFX} from './flanger/flanger.fx.js';
 
 export default [
   DelayFX,
   ReverbFX,
-  ParametreqFX
+  ParametreqFX,
+  FlangerFX
 ];
